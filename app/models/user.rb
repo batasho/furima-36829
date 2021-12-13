28268b5aaf9acc65c1a16f5fcc7932932a85c935
batasho <mcfly.s1211@gmail.com>
2021-12-13 17:32:46 +0900

    ユーザー管理機能（修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/,message: "半角英数を両方含む必要があります"}
-  validates :first_name, presence: true ,format: { with: /\A[ぁ-んァ-ンー-龥]+\z/,message: "全角ひらがな、カタカナ、漢字のみが使えます" }
-  validates :last_name, presence: true ,format: { with: /\A[ぁ-んァ-ンー-龥]+\z/,message: "全角ひらがな、カタカナ、漢字のみが使えます" }
+  validates :first_name, presence: true ,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,message: "全角ひらがな、カタカナ、漢字のみが使えます" }
+  validates :last_name, presence: true ,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,message: "全角ひらがな、カタカナ、漢字のみが使えます" }
   validates :kfirst_name, presence: true ,format: { with: /\A[ァ-ヶー－]+\z/,message: "全角カタカナのみが使えます" }
   validates :klast_name, presence: true ,format: { with: /\A[ァ-ヶー－]+\z/,message: "全角カタカナのみが使えます" }
   validates :birthday, presence: true

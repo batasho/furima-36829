35c07afd1286ffa36597028e25c40287a39625b4
batasho <mcfly.s1211@gmail.com>
2021-12-13 17:14:04 +0900

    ユーザー管理機能（修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/,message: "半角英数を両方含む必要があります"}
-  validates :first_name, presence: true ,format: { with: /\A[ぁ-んァ-ン一-龥]/,message: "全角ひらがな、カタカナ、漢字のみが使えます" }
-  validates :last_name, presence: true ,format: { with: /\A[ぁ-んァ-ン一-龥]/,message: "全角ひらがな、カタカナ、漢字のみが使えます" }
+  validates :password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/,message: "半角英数を両方含む必要があります"}
+  validates :first_name, presence: true ,format: { with: /\A[ぁ-んァ-ンー-龥]+\z/,message: "全角ひらがな、カタカナ、漢字のみが使えます" }
+  validates :last_name, presence: true ,format: { with: /\A[ぁ-んァ-ンー-龥]+\z/,message: "全角ひらがな、カタカナ、漢字のみが使えます" }
   validates :kfirst_name, presence: true ,format: { with: /\A[ァ-ヶー－]+\z/,message: "全角カタカナのみが使えます" }
   validates :klast_name, presence: true ,format: { with: /\A[ァ-ヶー－]+\z/,message: "全角カタカナのみが使えます" }
   validates :birthday, presence: true

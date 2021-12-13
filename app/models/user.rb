85a8888e4eaebc2ed4d35d3e9939c4fd3e530a39
batasho <mcfly.s1211@gmail.com>
2021-12-13 16:19:48 +0900

    ユーザー管理機能（バリデーション、単体コードの修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :kfirst_name, presence: true
-  validates :klast_name, presence: true
+  validates :password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/,message: "半角英数を両方含む必要があります"}
+  validates :first_name, presence: true ,format: { with: /\A[ぁ-んァ-ン一-龥]/,message: "全角ひらがな、カタカナ、漢字のみが使えます" }
+  validates :last_name, presence: true ,format: { with: /\A[ぁ-んァ-ン一-龥]/,message: "全角ひらがな、カタカナ、漢字のみが使えます" }
+  validates :kfirst_name, presence: true ,format: { with: /\A[ァ-ヶー－]+\z/,message: "全角カタカナのみが使えます" }
+  validates :klast_name, presence: true ,format: { with: /\A[ァ-ヶー－]+\z/,message: "全角カタカナのみが使えます" }
   validates :birthday, presence: true
-  has_many :stocks
-  has_many :purchases
+  #has_many :stocks
+  #has_many :purchases
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,7 +15,6 @@ RSpec.describe User, type: :model do
       it 'nicknameが空だと登録できない' do
         @user.nickname = ''
         @user.valid?
-        # binding.pry
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
       it 'emailが空だと登録できない' do
@@ -70,6 +69,49 @@ RSpec.describe User, type: :model do
         @user.password_confirmation = '00000'
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+      it 'emailに＠を含まない場合登録できない' do
+        @user.email = 'test.gmail.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it 'passwordに全角文字を含むと登録できない' do
+        @user.password = '１２３qwe'
+        @user.password_confirmation = '１２３qwe'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password 半角英数を両方含む必要があります")
+      end
+      it 'passwordが英字のみだと登録できない' do
+        @user.password = 'qweasd'
+        @user.password_confirmation = 'qweasd'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password 半角英数を両方含む必要があります')
+      end
+      it 'passwordが数字のみだと登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password 半角英数を両方含む必要があります')
+      end
+      it 'first_nameに半角文字が含まれていると登録できない' do
+        @user.first_name = 'test'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name 全角ひらがな、カタカナ、漢字のみが使えます")
+      end
+      it 'last_nameに半角文字が含まれていると登録できない' do
+        @user.last_name = 'taro'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name 全角ひらがな、カタカナ、漢字のみが使えます")
+      end
+      it 'kfirst_nameにカタカナ以外の文字が含まれていると登録できない' do
+        @user.kfirst_name = 'test'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Kfirst name 全角カタカナのみが使えます")
+      end
+      it 'klast_nameにカタカナ以外の文字が含まれていると登録できない' do
+        @user.klast_name = 'taro'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Klast name 全角カタカナのみが使えます")
       end
     end
   end
